05a4bfc2046c1e07254f94a90ff14e3f4ca30ad5
M2L <msirp@foxmail.com>
2014-06-24 18:14:25 +0200

    Deleted

--- src/indexNodesDBUtils/DBException.java
+++
@@ -1,9 +0,0 @@
-package indexNodesDBUtils;
-
-import java.sql.SQLException;
-
-public class DBException extends SQLException {
-
-	private static final long serialVersionUID = 1987199296442440185L;
-
-}
