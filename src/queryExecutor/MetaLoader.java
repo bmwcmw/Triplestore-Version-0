ba1948dcba8acb8f6fc49c267f7b0a0b7bf5a663
MW CHEN <900930@gmail.com>
2014-08-05 19:22:05 +0200

    Prepare

---
+++ src/queryExecutor/MetaLoader.java
@@ -0,0 +1,15 @@
+package queryExecutor;
+
+/**
+ * Detects and loads meta-data of compressed predicate files (block mode)
+ * @author UTILISATEUR
+ *
+ */
+public class MetaLoader {
+	
+	public static BTree load(String predpath){
+		BTree loaded = new BTree();
+		return loaded;
+	}
+
+}
