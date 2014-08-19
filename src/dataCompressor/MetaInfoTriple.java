6f022fde055ca78a2dc43c160961e3144890eb92
CMW <900930@gmail.com>
2014-08-20 00:39:11 +0200

    modify note

--- src/dataCompressor/MetaInfoTriple.java
+++ src/dataCompressor/MetaInfoTriple.java
@@ -2,7 +2,7 @@ package dataCompressor;
 
 /**
  * Data structure for a meta information (block mode)
- * Number of file, First occured S/O's ID, Offset of O/S (0 or last O/S of last file).
+ * File ID (last number), First occured S/O's ID, Offset of O/S (0 or last O/S of last file).
  * @author Cedar
  *
  */
