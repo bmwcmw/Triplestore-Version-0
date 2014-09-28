1e022154cd475f957b70f60eeadf256df007efd5
CMW <900930@gmail.com>
2014-09-28 19:16:41 +0200

    well...

--- CtmPreProcessingClient/src/dataCompressorUtils2/InRamDBUtils2Test.java
+++ CtmPreProcessingClient/src/dataCompressorUtils2/InRamDBUtils2Test.java
@@ -1,43 +1,37 @@
 package dataCompressorUtils2;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-
-import localIOUtils.IOUtils;
-
-import ctmRdf.CTMConstants;
-import ctmRdf.CTMJobAssigner;
-import ctmRdf.CTMServerConfig;
-
 public class InRamDBUtils2Test {
 	public final static String _workingDir = System.getProperty("user.dir");
 	
 	public static void main(String[] args) throws Exception {
+		InRamDBUtils2 dbu = new InRamDBUtils2(args[0], args[1]);
+		boolean auxIndex = Boolean.getBoolean(args[2]);
+		dbu.loadAuxIndexFromFile();
+		dbu.compress(auxIndex);
 		
-//		CTMServerConfig myconfig = CTMServerConfig.getInstance();
-//		myconfig._nbThreads = 16;
-		String psPath = _workingDir + File.separator + ".." + File.separator + "CtmDataSet" 
-				+ File.separator + "_psFake_sorted" + File.separator;
-		File folder = new File(psPath);
-		ArrayList<File> listOfFiles = new ArrayList<File>(Arrays.asList(folder.listFiles()));
-//		ArrayList<ArrayList<File>> inputLists = CTMJobAssigner.assignJobs(listOfFiles, false);
-		
-		String outputPath = _workingDir + File.separator + "_compressed";
-		
-		HashSet<String> doneList = new HashSet<String>();
-		
-		for(File f : listOfFiles) {
-			String tempFName = IOUtils.filenameWithoutExt(f.getAbsolutePath());
-			if(!doneList.contains(tempFName)) {
-				doneList.add(tempFName);
-				System.out.println(IOUtils.filenameWithoutExt(f.getAbsolutePath()) 
-						+ CTMConstants.SOSortedExt);
-				System.out.println(IOUtils.filenameWithoutExt(f.getAbsolutePath()) 
-						+ CTMConstants.OSSortedExt);
-			}
-		}
+////		CTMServerConfig myconfig = CTMServerConfig.getInstance();
+////		myconfig._nbThreads = 16;
+//		String psPath = _workingDir + File.separator + ".." + File.separator + "CtmDataSet" 
+//				+ File.separator + "_psFake_sorted" + File.separator;
+//		File folder = new File(psPath);
+//		ArrayList<File> listOfFiles = new ArrayList<File>(Arrays.asList(folder.listFiles()));
+////		ArrayList<ArrayList<File>> inputLists = CTMJobAssigner.assignJobs(listOfFiles, false);
+//		
+//		String outputPath = _workingDir + File.separator + "_compressed";
+//		
+//		HashSet<String> doneList = new HashSet<String>();
+//		
+//		for(File f : listOfFiles) {
+//			String tempFName = IOUtils.filenameWithoutExt(f.getAbsolutePath());
+//			if(!doneList.contains(tempFName)) {
+//				doneList.add(tempFName);
+////				System.out.println(IOUtils.filenameWithoutExt(f.getAbsolutePath()) 
+////						+ CTMConstants.SOSortedExt);
+////				System.out.println(IOUtils.filenameWithoutExt(f.getAbsolutePath()) 
+////						+ CTMConstants.OSSortedExt);
+//				
+//			}
+//		}
 		
 //		String inputFilePath = _workingDir + File.separator + ".." + File.separator + "CtmDataSet" 
 //				+ File.separator + "_psFake_sorted" + File.separator + "hasAuthor" + CTMConstants.SOSortedExt;
